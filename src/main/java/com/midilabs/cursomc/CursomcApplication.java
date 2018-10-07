e5348cefda012a2977bdc2553039003a3f46760b
Marcus Durao <mvdurao@gmail.com>
2018-10-07 18:17:57 -0300

    Produto associacao muito para muitos

--- src/main/java/com/midilabs/cursomc/CursomcApplication.java
+++ src/main/java/com/midilabs/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.midilabs.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.midilabs.cursomc.domain.Categoria;
+import com.midilabs.cursomc.domain.Produto;
 import com.midilabs.cursomc.repository.CategoriaRepository;
+import com.midilabs.cursomc.repository.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	CategoriaRepository categoriaRepository;
-	
+
+	@Autowired
+	ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -25,9 +29,22 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
-	
-	
+
 }
