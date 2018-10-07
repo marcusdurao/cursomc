05668a1fd2937f7c7807f7d37a4a644785271c2c
Marcus Durao <mvdurao@gmail.com>
2018-10-07 17:19:36 -0300

    Criando repository e servie para categoria

--- src/main/java/com/midilabs/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/midilabs/cursomc/resources/CategoriaResource.java
@@ -3,28 +3,30 @@ package com.midilabs.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.midilabs.cursomc.domain.Categoria;
+import com.midilabs.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
+
+	@Autowired
+	private CategoriaService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+
+		Categoria obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
-		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritorio");
-		
-		List<Categoria> categorias = new ArrayList<Categoria>();
-		
-		categorias.add(cat1);
-		categorias.add(cat2);
-		
-		return categorias;
 	}
 
 }
